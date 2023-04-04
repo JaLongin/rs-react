5f5b9fd5e39aff6fe250340e0dd5570514435d7f
JaLongin <neznayupochemu1@gmail.com>
2023-04-05 00:44:10 +0300

    refactor: rewrite forms page  with hooks

--- react-hooks/src/pages/Forms.tsx
+++ react-hooks/src/pages/Forms.tsx
@@ -1,38 +1,23 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Form } from "../components/Form";
 import FormCard from "../components/FormCard";
 import Cards from "../util/types/Cards";
 
-type FormState = {
-  cards: Cards[];
-};
-
-class Forms extends Component<object, FormState> {
-  constructor(props: object) {
-    super(props);
-    this.state = {
-      cards: [],
-    };
-    this.updateFormsPage = this.updateFormsPage.bind(this);
+function Forms() {
+  const [pageState, setFormPageState] = useState([] as Cards[]);
+  function updateFormsPage(card: Cards) {
+    setFormPageState([...pageState, card]);
   }
-  updateFormsPage(card: Cards) {
-    this.setState((prevState) => ({
-      cards: [...prevState.cards, card],
-    }));
-  }
-  render() {
-    return (
+  return (
+    <div>
+      <h2>Forms</h2>
+      <Form updateFormPage={updateFormsPage} />
       <div>
-        <h2>Forms</h2>
-        <Form updateFormPage={this.updateFormsPage} />
-        <div>
-          {this.state.cards.map((card) => (
-            <FormCard key={card.name} data={card} />
-          ))}
-        </div>
+        {pageState.map((card) => (
+          <FormCard key={card.name} data={card} />
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
 }
-
 export default Forms;
